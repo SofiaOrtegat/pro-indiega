9f2ec690e5f178d52c86a73b2b59ed85ded5bc69
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-01-05 17:00:49 +0000

    Test updates.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.sass']
 })
-export cl
+export class AboutComponent {
+
+}
---
+++ src/app/about/about.module.ts
@@ -0,0 +1,3 @@
+import { CountsComponent } from './counts/counts.component';
+import { SummaryComponent } from './summary/summary.component';
+import { NgModule } from '@angular/co
