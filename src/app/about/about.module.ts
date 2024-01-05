502413af3dc366ff6b8802614f5a7ee73600b327
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-01-05 17:30:55 +0000

    format updates

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,3 +1,7 @@
 import { CountsComponent } from './counts/counts.component';
 import { SummaryComponent } from './summary/summary.component';
-import { NgModule } from '@angular/co
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AboutRoutingModule } from './about-routing.module';
+import { AboutComponent } from './about.component';
