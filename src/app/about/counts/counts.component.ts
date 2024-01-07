2eac29892aec87d756b55738d1d3401ead53c02b
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-01-07 11:32:05 +0000

    Removed build files

---
+++ src/app/about/counts/counts.component.ts
@@ -0,0 +1,20 @@
+
+import { StatsService } from './../../shared/stats.service';
+import { Features } from './../../shared/stats.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: "app-counts",
+  templateUrl: "./counts.component.html",
+  styleUrls: ["./counts.component.sass"]
+})
+export class CountsComponent implements OnInit {
+  features: Array<Features> = []
+  constructor(private statsService: StatsService) {}
+
+  ngOnInit(): void {
+    this.statsService
+      .getStats()
+      .then(features => this.features = features)
+  }
+}
