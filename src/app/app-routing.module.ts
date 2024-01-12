2a42359b21130bf89cf77ce3ce931e1b45d968de
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-01-12 20:54:37 +0000

    Explicitly replace "import tensorflow" with "tensorflow.compat.v1"

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './homepage/homepage.module#HomepageModule',
+    data: {breadrumb: 'Home'}
+  },
+  {
+    path: 'about-us',
+    loadChildren: './about/about.module#AboutModule',
+    data: {breadcrumb: 'About'}
+  },
+  {
+    path: 'faq',
+    loadChildren: './core/core.module#CoreModule',
+    data: { breadcrumb: 'Core Values' }
+  },
+  {
+    path: 'gallery',
+    loadChildren: './gallery/gallery.module#GalleryModule',
+    data: { breadcrumb: 'Gallery' }
+  },
+  {
+    path: '**',
+    loadChildren: './global/not-found/not-found.module#NotFoundModule'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
