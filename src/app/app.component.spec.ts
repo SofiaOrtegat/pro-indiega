2b6b7c9f62d92f1f5b1e3c8b24674e270909df65
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-01-16 17:20:59 +0000

    removed useless files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,2 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@a
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+    
