0fac2dd7892ca885db7d47947cf7bcfca36b0721
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-01-18 08:38:43 +0000

    supervisor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,4 +3,14 @@ import { SharedModule } from '../global/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CoreRou
+import { CoreRoutingModule } from './core-routing.module';
+
+@NgModule({
+  declarations: [CoreComponent],
+  imports: [
+    SharedModule,
+    CommonModule,
+    CoreRoutingModule
+  ]
+})
+export class Co
