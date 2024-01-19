6a6d7703f5e21d2da46a7a902902621a03b45584
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-01-19 18:22:40 +0000

    Corrected some typos

--- src/app/gallery/gallery/gallery.component.ts
+++ src/app/gallery/gallery/gallery.component.ts
@@ -1,2 +1,8 @@
 import { Image } from './../../shared/images.model';
-import { ImagesService } from './../../shared/images.service'
+import { ImagesService } from './../../shared/images.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gal
