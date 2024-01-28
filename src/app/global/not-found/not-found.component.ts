c30718a7a497e4587c5f39dad84e956884d7eb24
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-01-28 13:40:24 +0000

    net: add a sysctl to reflect the fwmark on replies

--- src/app/global/not-found/not-found.component.ts
+++ src/app/global/not-found/not-found.component.ts
@@ -5,4 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.sass']
 })
-export class Not
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/global/not-found/not-found.module.ts
@@ -0,0 +1,3 @@
+import { SharedModule } from './../shared.module';
+import { NgModule } from '@angular/core';
+im
