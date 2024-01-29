7513842d627c76244e4d8569b8ebadead3a4c9e9
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-01-29 22:13:30 +0000

    Update ip.txt

--- src/app/global/not-found/not-found.module.ts
+++ src/app/global/not-found/not-found.module.ts
@@ -5,3 +5,14 @@ import { CommonModule } from '@angular/common';
 import { NotFoundRoutingModule } from './not-found-routing.module';
 import { NotFoundComponent } from './not-found.component';
 
+@NgModule({
+  declarations: [
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    NotFoundRoutingModule,
+    SharedModule
+  ]
+})
+export class NotF
