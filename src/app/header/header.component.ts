8aba52f23d3c56e9aeb2ed4a089c9d00d65e0a38
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-02-02 12:38:52 +0000

    moved to core

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,4 +6,9 @@ import {
   HostListener,
   Inject,
   OnInit,
-  
+  Renderer2,
+  ViewChild
+} from "@angular/core";
+import { MatToolbar } from "@angular/material";
+import { DOCUMENT } from "@angular/common";
+import { WINDOW } from "../sha
