c5201f0e2f4ee3084a19325029e5147b446486df
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-02-17 06:19:12 +0000

    modal

--- src/app/homepage/upcoming/upcoming.component.ts
+++ src/app/homepage/upcoming/upcoming.component.ts
@@ -10,4 +10,13 @@ export class UpcomingComponent {
   tiles = [
     {
       title: "Title Sample #1",
-      date: "Text 
+      date: "Text Sample #1",
+      image: "assets/images/game-img-1.jpg"
+    },
+    {
+      title: "Title Sample #2",
+      date: "Text Sample #2",
+      image: "assets/images/game-img-2.jpg"
+    },
+    {
+      title: "
