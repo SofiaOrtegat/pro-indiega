55ada1b2607891e1a59093a1fb4125998494462a
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-02-21 08:08:25 +0000

    Added screenshots.

--- src/app/mock-db/mock-core.ts
+++ src/app/mock-db/mock-core.ts
@@ -3,4 +3,5 @@ import { CoreValues } from "../shared/core.model";
 export const CORE_VALUES: Array<CoreValues> = [
   {
     title: `We’re after World 1st`,
-    subtitle: `
+    subtitle: ``,
+    text: `We’re not here to help you collect pink pixels, we’re not here so you can brag to randos about what a badass you are. We’re here to compete. That’s it
