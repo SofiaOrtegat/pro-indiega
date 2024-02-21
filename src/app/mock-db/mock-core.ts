a552286123839d662826447c60d034ca43253c92
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-02-21 16:13:04 +0000

    Initial Build

--- src/app/mock-db/mock-core.ts
+++ src/app/mock-db/mock-core.ts
@@ -4,4 +4,9 @@ export const CORE_VALUES: Array<CoreValues> = [
   {
     title: `We’re after World 1st`,
     subtitle: ``,
-    text: `We’re not here to help you collect pink pixels, we’re not here so you can brag to randos about what a badass you are. We’re here to compete. That’s it
+    text: `We’re not here to help you collect pink pixels, we’re not here so you can brag to randos about what a badass you are. We’re here to compete. That’s it.`
+  },
+  {
+    title: `Talent First`,
+    subtitle: ``,
+    text: `There is no set of skills a person can have that will allow for them to compensate for a la
