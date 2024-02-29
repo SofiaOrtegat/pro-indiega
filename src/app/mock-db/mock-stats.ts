15c72c4d864460259228050033ac7801f8709224
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-02-29 10:38:03 +0000

    HTTPS

--- src/app/mock-db/mock-stats.ts
+++ src/app/mock-db/mock-stats.ts
@@ -1,3 +1,7 @@
 import { Features } from "../shared/stats.model";
 import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
-import { faGlobeAmericas, faTrophy, faUsers } from "
+import { faGlobeAmericas, faTrophy, faUsers } from "@fortawesome/free-solid-svg-icons";
+
+export const STATS: Array<Features> = [
+  { icon: faThumbsUp, count: 12356, source: 'happy clients' },
+  { icon: faTrophy, count: 2356, source: 'W
