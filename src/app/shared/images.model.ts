684223379cc4d077eb1864600d61cd95fd1ca626
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-02-29 13:07:03 +0000

    br: fix use of ->rx_handler_data in code executed on non-rx_handler path

--- src/app/shared/images.model.ts
+++ src/app/shared/images.model.ts
@@ -2,3 +2,4 @@
 export interface Image {
   imageUrl: string
   alt: string
+}
---
+++ src/app/shared/images.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { IMAGES } from '../mock-db/mock-images';
+import { Image } from './images.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImagesServi
