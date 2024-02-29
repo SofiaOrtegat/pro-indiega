dca144c7198418a14a5d3f035e08556760d69e7b
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-02-29 09:45:13 +0000

    Add migrations

--- src/app/mock-db/mock-images.ts
+++ src/app/mock-db/mock-images.ts
@@ -11,3 +11,11 @@ export const IMAGES: Array<Image> = [
   { imageUrl: '../../assets/images/footer-bg.jpg', alt: 'Footer Background' },
   { imageUrl: '../../assets/images/game-img-1.jpg', alt: 'Game Image 1' },
   { imageUrl: '../../assets/images/game-img-2.jpg', alt: 'Game Image 2' },
+  { imageUrl: '../../assets/images/game-img-3.jpg', alt: 'Game Image 3' },
+  { imageUrl: '../../assets/images/game-img-4.jpg', alt: 'Game Image 4' },
+  { imageUrl: '../../assets/images/game-slide-img-1.jpg', alt: 'Game Slide image' },
+  { imageUrl: '../../assets/images/page-name.jpg', alt: 'Page Name' },
+  { imageUrl: '../../assets/images/slide-1.jpg', alt: 'Slide 1' },
+  { imageUrl: '../../assets/images/slide-2.jpg', alt: 'Slide 2' },
+  { imageUrl: '../../assets/images/slide-3.jpg', alt: 'Slide 3' }
+]
---
+++ src/app/mock-db/mock-stats.ts
@@ -0,0 +1,3 @@
+import { Features } from "../shared/stats.model";
+import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
+import { faGlobeAmericas, faTrophy, faUsers } from "
