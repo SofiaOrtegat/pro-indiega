03189b76ae2e66f21d739e961b59b6f817d23ccc
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-02-29 14:56:23 +0000

    fixes #20

--- src/app/shared/links.service.ts
+++ src/app/shared/links.service.ts
@@ -1,1 +1,7 @@
-import { DROPDOWN_
+import { DROPDOWN_LINKS, LINKS } from './../mock-db/mock-links';
+import { Links } from './links.model';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LinksService {
+ 
