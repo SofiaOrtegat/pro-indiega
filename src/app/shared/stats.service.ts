3cd2a3ef46ea969b058b518f380a51b6dd77261d
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-03-01 08:46:15 +0000

    Bumped version, updated changelog

--- src/app/shared/stats.service.ts
+++ src/app/shared/stats.service.ts
@@ -1,2 +1,9 @@
 import { Injectable } from '@angular/core';
-import { STATS } from '../mock-db
+import { STATS } from '../mock-db/mock-stats';
+import { Features } from './stats.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatsService {
+  // tslint:disable-next-line:array-
