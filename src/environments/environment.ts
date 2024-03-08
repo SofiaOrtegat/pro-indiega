9dc8678251875013ace89cc7689497a45eedf697
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-03-08 11:12:40 +0000

    Bump version to 1.3.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements ca
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the f
