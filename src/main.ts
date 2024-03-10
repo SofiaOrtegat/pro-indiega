d002a7f0d2c04c21e2954043b6faa068e8870879
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-03-10 09:27:31 +0000

    Update stoichiometry.py

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './ap
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bo
