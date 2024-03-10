57a36025d35961ba0ab8af2998f01ab5142da238
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-03-10 06:51:50 +0000

    1.5.11

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-import 'hamme
+import 'hammerjs';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './ap
