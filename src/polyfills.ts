e3d261fa0c54c7b29b58146edf7c818415256413
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-03-12 08:58:58 +0000

    fixed URL

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -14,4 +14,9 @@
  * Learn more in https://angular.io/guide/browser-support
  */
 
-/******************************************************
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+// import '
