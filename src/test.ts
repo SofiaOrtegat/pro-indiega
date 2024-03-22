8041e2f9e805792f8b498739fd83d721c4aa3e01
SofiaOrtegat <SofiaOrtegat@proton.me>
2024-03-22 14:07:41 +0000

    net: bridge: convert MLDv2 Query MRC into msecs_to_jiffies for max_delay

--- src/test.ts
+++ src/test.ts
@@ -15,4 +15,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const contex
+const context = require.context('./', true, /\.spec\.ts$/);
+// And load the modules.
+context.keys().map(context);
